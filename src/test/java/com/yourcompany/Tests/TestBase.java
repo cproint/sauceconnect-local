a9ff0ef85feb652b1f631b56671acfa6c72ccaa3
cproint <cproint@gmail.com>
2018-08-28 21:23:46 -0700

    typo in Jenkinsfile

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -134,8 +134,19 @@ public class TestBase  {
                 capabilities));
 
         // set current sessionId
-        String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
-        sessionId.set(id);
+        //String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+        //sessionId.set(id);
+        
+        
+        
+        	 
+            String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
+            (((RemoteWebDriver) getWebDriver()).getSessionId().toString()), "some job name");
+            System.out.println(message);
+        
+        
+        
+        
     }
 
     /**
